bb19626d5ed8797372dd1ef6c5aa3b524e8cd511
SSSenia <semen.sinitsin@gmail.com>
2022-12-09 15:46:35 +0200

    changed architecture for help articles

--- src/app/content/help-page/help-page.component.ts
+++ src/app/content/help-page/help-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ARTICLES } from 'src/app/shared/articles';
+import { IArticle } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-help-page',
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.scss']
 })
-export class HelpPageComponent implements OnInit {
+export class HelpPageComponent {
+
+  public atricles: IArticle[] = ARTICLES;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
